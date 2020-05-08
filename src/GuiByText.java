46c119933906998392b55eb323e75a06b1f5128f
dhafinharish <DHaris454@gmail.com>
2020-05-08 13:14:55 +0800

    javax_swing

---
+++ src/GuiByText.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class GuiByText extends JFrame {
+    GuiByText(){
+        setsize( width: 640, height: 480);
+    }
+
+    public static void main(String[] args) {
+        GuiByText guiByText = new GuiByText();
+        guiByText.setVisible(true);
+    }
+}
